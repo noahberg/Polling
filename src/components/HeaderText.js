cbaa9c84c903544a3c874621eb881f6e83f3137f
Drew Osherow <dosherow@usc.edu>
2019-05-16 02:30:30 -0700

    beginning of homepage

---
+++ src/components/HeaderText.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.div`
+    font-family: ${props => (props.primary ? "Gill Sans" : "Marion")};
+    font-size: ${props => (props.primary ? "1.8vw" : "3.5vw")};
+    padding: ${props => (props.primary ? "-0.4vh 0vh" : "1vh 0vh")};
+    line-height: ${props => (props.primary ? "0vh" : "6vh")};
+    font-weight: ${props => (props.primary ? "regular" : "bold")};
+    color: white;
+    align-self: ${props => (props.primary ? "center" : "left")};
+    justify-self: ${props => (props.primary ? "center" : "left")};
+`;
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.button`
+    border-radius: .8vw;
+    border-width: .2vw;
+    border-color: ${props => (props.butn ? "white" : "transparent")};
+    background-color: transparent;
+
+`;
