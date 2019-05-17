ed26f4bdf795dd926cdd84ecbf547fa98312dc64
Drew Osherow <dosherow@usc.edu>
2019-05-17 01:27:16 -0700

    Removed duplicate files

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default styled.header`
-    background-color: transparent;
-    display: grid;
-    grid-template-columns: 4vw 30vw auto 11vw 11vw 11vw 11vw;
-    text-align: center;
-    padding: 6vh 5vw;
-`;
